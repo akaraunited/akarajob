1f1c169838f1e83657a662530b12a4e3bf5dbf5e
Borey Da <daborey@gmail.com>
2020-06-02 23:55:33 +0700

    update homepage and add header and footer use bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,125 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AkaraJob - Homepage</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AkaraJob - Homepage</title>
+
+<!-- bootstrap library import -->
+<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- internal style import -->
+<link rel="stylesheet" href="{{asset('css/style.css') }}">
+
+</head> 
 <body>
-    <main>
+<nav class="navbar navbar-default navbar-expand-lg navbar-light">
+	<div class="navbar-header d-flex col">
+		<a class="navbar-brand" href="#"><img src="{{asset('images/logo.png')}}" alt="" style="width: 56px; height: 56px;"></a>  		
+		<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle navbar-toggler ml-auto">
+			<span class="navbar-toggler-icon"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	</div>
+	<!-- Collection of nav links, forms, and other content for toggling -->
+	<div id="navbarCollapse" class="collapse navbar-collapse justify-content-start">
+		<ul class="nav navbar-nav">
+			<li class="nav-item"><a href="#" class="nav-link">Jobs</a></li>
+			<li class="nav-item"><a href="#" class="nav-link">How to write CV</a></li>
+			<li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+			<!-- <li class="nav-item dropdown">
+				<a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#">Services <b class="caret"></b></a>
+				<ul class="dropdown-menu">					
+					<li><a href="#" class="dropdown-item">Web Design</a></li>
+					<li><a href="#" class="dropdown-item">Web Development</a></li>
+					<li><a href="#" class="dropdown-item">Graphic Design</a></li>
+					<li><a href="#" class="dropdown-item">Digital Marketing</a></li>
+				</ul>
+			</li> -->
+		</ul>
+		<!-- <form class="navbar-form form-inline">
+			<div class="input-group search-box">								
+				<input type="text" id="search" class="form-control" placeholder="Search here...">
+				<span class="input-group-addon"><i class="material-icons">&#xE8B6;</i></span>
+			</div>
+		</form> -->
+		<ul class="nav navbar-nav navbar-right ml-auto">			
+			<li class="nav-item">
+				<a data-toggle="dropdown" class="nav-link dropdown-toggle" href="#">Login</a>
+				<ul class="dropdown-menu form-wrapper">					
+					<li>
+						<form action="/examples/actions/confirmation.php" method="post">
+							<p class="hint-text" style="text-align: left;">Login</p>
+							<!-- <div class="form-group social-btn clearfix">
+								<a href="#" class="btn btn-primary pull-left"><i class="fa fa-facebook"></i> Facebook</a>
+								<a href="#" class="btn btn-info pull-right"><i class="fa fa-twitter"></i> Twitter</a>
+							</div> -->
+							<!-- <div class="or-seperator"><b>or</b></div> -->
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Email" required="required">
+							</div>
+							<div class="form-group">
+								<input type="password" class="form-control" placeholder="Password" required="required">
+							</div>
+                            <input type="submit" class="btn btn-primary btn-block" value="Login">
+							<div class="form-footer">
+								<p>Login via
+									<a href="#" class=""><i class="fa fa-facebook"></i> Facebook</a>
+								</p>
+								<a href="#">Forgot Your password?</a>
+							</div>
+						</form>
+					</li>
+				</ul>
+			</li>
+			<li class="nav-item">
+				<a href="#" data-toggle="dropdown" class="btn btn-primary dropdown-toggle get-started-btn mt-1 mb-1">Sign up</a>
+				<ul class="dropdown-menu form-wrapper">					
+					<li>
+						<form action="/examples/actions/confirmation.php" method="post">
+							<p class="hint-text" style="text-align: left;">Register an Account</p>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="First Name" required="required">
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Last Name" required="required">
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Email" required="required">
+							</div>
+							<div class="form-group">
+								<input type="password" class="form-control" placeholder="Password" required="required">
+							</div>
+							<div class="form-group">
+								<input type="password" class="form-control" placeholder="Confirm Password" required="required">
+							</div>
+							<!-- <div class="form-group">
+								<label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms &amp; Conditions</a></label>
+							</div> -->
+							<input type="submit" class="btn btn-primary btn-block" value="Sign up">
+							<div class="form-footer">
+								<p>Register via
+									<a href="#" class=""><i class="fa fa-facebook"></i> Facebook</a>
+								</p>
+								
+							</div>
+						</form>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<main>
     <div class="container" >
     <ul class="list-group">
         <h4>List Lastest Job</h4>
@@ -28,7 +138,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -49,7 +159,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -70,7 +180,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -91,7 +201,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -112,7 +222,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -133,7 +243,7 @@
                     <h5><strong>Full-Time</strong></h5>
                 </div>
                 <div class="panel-more1">
-                    <img src={{ asset('images/wing.png') }} />
+                    <img src='{{asset('images/wing.png') }}'/>
                     <br /><span>company : Wing Bank</span>
                 </div>
             </div>
@@ -142,10 +252,26 @@
 </ul>
 </div>
     </main>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="footer-col-rt col-md-6">
+                    <p>Contact Us</p>
+                    <p>akarajob@gmail.com / 087479956</p>
+                    <p><a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-skype"></a>
+                        <a href="#" class="fa fa-youtube"></a>
+                    </p>
+                </div>
+                <div class="footer-col-lf col-md-6">
+                    <p>&copy; 2020 by AkaraJob</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<!-- javascript script import -->
+<script type="text/javascript" src="script.js"></script>
 </body>
-</html>
+</html>                                                        
