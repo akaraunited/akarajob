67a974cfef446c8c68dbbc0c8dd5daa426d2e9f3
Borey Da <daborey@gmail.com>
2020-06-02 22:29:26 +0700

    add main

---
+++ resources/views/index.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AkaraJob - Homepage</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <main>
+    <div class="container" >
+    <ul class="list-group">
+        <h4>List Lastest Job</h4>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-info">
+                    <h5><strong>Executive Manager</strong></h5>
+                    <h6><strong>salary : 1500 $</strong></h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Phnom Penh</strong></h5>
+                    <h6>expire : 12/06/2020</h6>
+                </div>
+                <div class="panel-more1">
+                    <h5><strong>Full-Time</strong></h5>
+                </div>
+                <div class="panel-more1">
+                    <img src={{ asset('images/wing.png') }} />
+                    <br /><span>company : Wing Bank</span>
+                </div>
+            </div>
+        </div>
+    </li>
+</ul>
+</div>
+    </main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
